391311dc87300de83ca532caea7a94d086ae789e
Diana Gakuya <deegakuya@gmail.com>
2023-10-02 21:29:37 +0300

    assignment 4

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const apiUrl = 'https://anapioficeandfire.com/api/books';
+
+  return fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      renderBooks(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
